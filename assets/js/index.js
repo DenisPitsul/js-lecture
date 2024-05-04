2183e77cd50fcce2e55dce6e4fb439a24825fd80
DenisPitsul <denia.pitsul@gmail.com>
2024-05-04 19:35:12 +0300

    variables, input, output, operations, casting types

---
+++ assets/js/index.js
@@ -0,0 +1,90 @@
+/* 
+alert("Hello World 1");
+alert("Hello World 2")
+*/
+
+// let age = 18;
+// alert(age);
+// age = 19;
+// alert(age);
+
+// const count = 10;
+// count = 11;
+// alert(count);
+
+// const PI = 3.14;
+
+// number
+// let n1 = 9999999999999999999999999999999999999;
+// alert(n1)
+
+// bigint
+// let bigNumber = 9999999999999999999999999999999999999n;
+// alert(bigNumber);
+
+// string
+// let phrase = "His name is 'Ivo'";
+// alert(phrase);
+// let someHtml = `
+//     <h1 style='color: red;'>Head</h1>
+//     <p>Paragraph.</p>
+// `;
+// document.write(someHtml);
+
+// boolean
+// let isEmail = true;
+// alert(email);
+// let gender = "male";
+// let isMale = false;
+
+// let name = "Ivo";
+// let email = "ivo@gmail.com"
+// let age = 23;
+// let isMale = true;
+// alert("Name " + name);
+// console.log("Name:", name, "\nEmail:", email)
+
+// null
+// let a = null;
+// alert(a);
+
+// undefined
+// let b;
+// alert(b);
+
+// symbol
+
+// not primitive object
+
+// let totalPrice = 2 * 500 + 3 * 200;
+// console.log("Total:", totalPrice, "hrn");
+
+// let amount1 = 2;
+// let price1 = 500;
+// let amount2 = 3;
+// let price2 = 200;
+// let totalPrice = amount1 * price1 + amount2 * price2;
+// console.log("Total:", totalPrice, "grn");
+
+// let firstItemPrice = 600;
+// let firstItemDiscount = 0.1;
+// let secondItemPrice = 800;
+// let secondItemDiscount = 0.15;
+// let orderPrice = firstItemPrice * (1 - firstItemDiscount) + secondItemPrice * (1 - secondItemDiscount);
+// console.log(orderPrice);
+
+// const firstName = prompt("Input name:", "anonimus");
+// const lastName = prompt("Input surname", "noname");
+
+// alert(firstName + " " + lastName);
+// document.write('<h2>' + firstName + " " + lastName + "</h2>");
+
+// const firstNumber = Number(prompt("Input first number:", "0"));
+// const secondNumber = +prompt("Input second number:", "0");
+// const sum = firstNumber + secondNumber;
+// console.log('sum:', sum);
+
+// const amountOfItems = +prompt("Input amount of items:");
+// const itemPrice = 200;
+// const orderPrice = amountOfItems * itemPrice;
+// console.log("Order price:", orderPrice);
