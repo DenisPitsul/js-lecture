8f22792e1b23ba021909a58b4007ad51c9009015
DenisPitsul <denia.pitsul@gmail.com>
2024-06-29 19:39:09 +0300

    Add RegExp part 1

---
+++ assets/js/regExp.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// /\br/g - r in the beggining of word
+// /\br/gi - r or R in the beggining or end of word
+// /\bwas\b/gi - str has 'was' word
+
+// *
+// ?
+// !
+// [A-Z]
+// [a-z]
+// [0-9]
+
+// const regExp1 = new RegExp("\\br", "gi");
+
+// const regExp2 = /\br/gi;
+
+// const testString = "rsdfsdf rsdfsdf dsvsdvdv";
+
+// // // regexp.test(string) => boolean
+// console.log(regExp2.test(testString));
+// console.log(testString.match(regExp2));
+
+// const string1 = "Test Testovich";
+// const reg1 = /^.{4,10} .{8,20}$/g;
+// console.log(string1.match(reg1));
+
+// const str1 = "f@f.com";
+// const str2 = "@dsfsdf.dfg.com";
+// const reg2 = /^.{1,10}@.{1,20}\..{2,4}$/g;
+// console.log(str1.match(reg2));
+// console.log(str2.match(reg2));
+
+// const text = "I have a dog and my friend has many dogs. I like hotdogs";
+// const reg3 = /\bdogs?\b/g;
+// console.log(text.match(reg3));
+
+// const name = "Elsa";
+// const nameReg = /^[A-Z][a-z]{1,31}$/;
+// console.log(name.match(nameReg));
